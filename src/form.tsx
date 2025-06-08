b6dc888c3ba71901219a85ba5c77caa068867696
Maria Sanli <marisanli.93@gmail.com>
2025-06-07 23:10:22 -0400

    setup: add react-hook-form dependency and create form interface

---
+++ src/form.tsx
@@ -0,0 +1,11 @@
+// import React from "react";
+import { useForm } from 'react-hook-form' // eslint-disable-line @typescript-eslint/no-unused-vars
+import './Form.css'
+
+// Business Name, Category, Location
+interface IformInput {
+  // eslint-disable-line @typescript-eslint/no-unused-vars
+  businessname: string
+  category: string
+  location: string
+}
