a1880d656738c7f5ad00427aa040075968ad1496
Maria Sanli <marisanli.93@gmail.com>
2025-06-10 21:16:54 -0400

    install yup resolver package

--- src/form.tsx
+++ src/form.tsx
@@ -1,11 +1,34 @@
-// import React from "react";
-import { useForm } from 'react-hook-form' // eslint-disable-line @typescript-eslint/no-unused-vars
+import React from 'react'
+import { useForm } from 'react-hook-form'
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import * yup from "yup";
 import './Form.css'
 
 // Business Name, Category, Location
 interface IformInput {
-  // eslint-disable-line @typescript-eslint/no-unused-vars
   businessname: string
   category: string
   location: string
+}
+
+export const Form: React.FC = () => {
+  const { register, handleSubmit } = useForm<IformInput>()
+
+  const onSubmit = (data: IformInput) => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Name</label>
+      <input {...register('businessname')} />
+
+      <label>Address</label>
+      <input {...register('category')} />
+
+      <label>Address</label>
+      <input {...register('location')} />
+      <input type="submit" />
+    </form>
+  )
 }
