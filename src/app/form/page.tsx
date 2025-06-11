d542805bfd1d0bc2342525bcb13f55d0204dbe96
Maria Sanli <marisanli.93@gmail.com>
2025-06-10 22:31:05 -0400

    feat: added form page to view

---
+++ src/app/form/page.tsx
@@ -0,0 +1,12 @@
+import { Form } from '@/form'
+
+export default function FormPage() {
+  return (
+    <div className="min-h-screen p-8">
+      <div className="max-w-md mx-auto">
+        <h1 className="text-2xl font-bold mb-6">Business Registration</h1>
+        <Form />
+      </div>
+    </div>
+  )
+}
