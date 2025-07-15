c07e16118d4f8b237bb725c63691de92a0da7904
Maria Sanli <marisanli.93@gmail.com>
2025-07-14 23:12:39 -0400

    feat: basic onboarding structure start

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+export default function OnboardingWizard() {
+  return (
+    <div className="min-h-screen bg-green-50 py-8">
+      <div className="max-w-4xl mx-auto px-6">
+        <h1>Onboarding Wizard</h1>
+      </div>
+    </div>
+  )
+}
