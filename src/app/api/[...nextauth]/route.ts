8007595c8dde0867425694e2e597f11cb8e9653d
Daun Kim <yennakim11@gmail.com>
2025-07-16 21:50:19 -0500

    chore: configured JWT session strategy

--- src/app/api/[...nextauth]/route.ts
+++ src/app/api/[...nextauth]/route.ts
@@ -2,6 +2,16 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 const handler = NextAuth({
+  session: {
+    strategy: 'jwt',
+    maxAge: 7 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60
+  },
+  jwt: {
+    maxAge: 7 * 24 * 60 * 60,
+    secret: process.env.NEXTAUTH_SECRET
+  },
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
