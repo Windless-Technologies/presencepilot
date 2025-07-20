e8b4fa2c761c6778ed3058d2bb6b32f82a00e7bd
Daun Kim <yennakim11@gmail.com>
2025-07-20 12:55:49 -0500

    chore: set up session callbacks

--- src/app/api/[...nextauth]/route.ts
+++ src/app/api/[...nextauth]/route.ts
@@ -17,7 +17,22 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
     })
-  ]
+  ],
+
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string
+      session.user.id = token.id
+      return session
+    }
+  }
 })
 
 export { handler as GET, handler as POST }
