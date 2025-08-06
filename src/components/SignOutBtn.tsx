7eff62cd0c706a6d9b745372398ebcb17026ed2a
Daun Kim <yennakim11@gmail.com>
2025-08-05 20:55:02 -0500

    chore(styling): sign out button color is red

--- src/components/SignOutBtn.tsx
+++ src/components/SignOutBtn.tsx
@@ -3,6 +3,11 @@ import { signOut } from 'next-auth/react'
 
 export default function SignOutBtn() {
   return (
-    <button onClick={() => signOut({ callbackUrl: '/login' })}>Sign out</button>
+    <button
+      className="bg-red-600 text-white py-2 px-4"
+      onClick={() => signOut({ callbackUrl: '/login' })}
+    >
+      Sign out
+    </button>
   )
 }
