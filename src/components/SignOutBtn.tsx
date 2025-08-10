a521bda733ae66fe31fe862ad741457cfa427418
Daun Kim <yennakim11@gmail.com>
2025-08-10 16:57:47 -0500

    style: sign out accessibility

--- src/components/SignOutBtn.tsx
+++ src/components/SignOutBtn.tsx
@@ -4,7 +4,8 @@ import { signOut } from 'next-auth/react'
 export default function SignOutBtn() {
   return (
     <button
-      className="bg-red-600 text-white py-2 px-4"
+      className="bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700 focus:ring-offset-2"
+      aria-label="Sign out of your account"
       onClick={() => signOut({ callbackUrl: '/login' })}
     >
       Sign out
